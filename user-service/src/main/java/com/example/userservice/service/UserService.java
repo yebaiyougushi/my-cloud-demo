e667bbf84f0c038d9d8fd3d94a6e9e03cf587971
23520055542@qq.com <2352005542@qq.com>
2021-10-09 21:20:05 +0800

    initial commit

---
+++ user-service/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.userservice.service;
+
+
+import com.example.userservice.mapper.UserMapper;
+import com.example.userservice.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return userMapper.findById(id);
+    }
+}
---
+++ user-service/src/main/java/com/example/userservice/web/UserController.java
@@ -0,0 +1,53 @@
+package com.example.userservice.web;
+
+
+import com.example.userservice.config.PatternProperties;
+import com.example.userservice.pojo.User;
+import com.example.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+// @RefreshScope
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // @Value("${pattern.dateformat}")
+    // private String dateformat;
+
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("prop")
+    public PatternProperties properties(){
+        return properties;
+    }
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    /**
+     * 路径： /user/110
+     *
+     * @param id 用户id
+     * @return 用户
+     */
+    @GetMapping("/{id}")
+    public User queryById(@PathVariable("id") Long id,
+                          @RequestHeader(value = "Truth", required = false) String truth) {
+        System.out.println("truth: " + truth);
+        User user = userService.queryById(id);
+        System.out.println(user);
+        return user;
+    }
+}
